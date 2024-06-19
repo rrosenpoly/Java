a3f8c947c6193f7dca3b88507fd94da73a231b8d
rrosenpoly <118927505+rrosenpoly@users.noreply.github.com>
2024-06-19 11:18:34 -0400

    Update BMI.java

--- Assignment5/BMI.java
+++ Assignment5/BMI.java
@@ -1,1 +1,34 @@
-
+class BMI {
+    private double weight, height;
+  
+    final double KILOGRAMS_PER_POUND = 0.45359237;
+    final double METERS_PER_INCH = 0.0254;
+  
+    public BMI(double weight, double height) {
+      this.weight = weight;
+      this.height = height;
+    }
+  
+    public double getBMI() {
+      double weightInKilograms = weight * KILOGRAMS_PER_POUND;
+      double heightInMeters = height * METERS_PER_INCH;
+      double bmi = weightInKilograms / (heightInMeters * heightInMeters);
+      return bmi;
+    }
+  
+    public String getStatus() {
+      double bmi = getBMI();
+      String status = "Normal";
+  
+      if (bmi < 18.5)
+        status = "Underweight";
+      else if (bmi < 25)
+        status = "Normal";
+      else if (bmi < 30)
+        status = "Overweight";
+      else
+        status = "Obese";
+  
+      return status;
+    }
+  }
