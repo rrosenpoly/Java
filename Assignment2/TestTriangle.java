358440a6e57fac0a90cab2feeef838847a16b1f0
rrosenpoly <118927505+rrosenpoly@users.noreply.github.com>
2024-06-19 11:09:55 -0400

    Update TestTriangle.java

--- Assignment2/TestTriangle.java
+++ Assignment2/TestTriangle.java
@@ -1,1 +1,14 @@
 
+public class TestTriangle {
+    public static void main(String[] args) {
+        Triangle c1 = null, c2 = null, c3 = null;
+        try {
+            c1 = new Triangle(3, 4, 5);
+            c2 = new Triangle(1, 2, 3);
+            c3 = new Triangle(10, 4, 5);
+        } catch (InvalidTriangleException ex) {
+            System.out.println(ex);
+        }
+            System.out.println("Created triangle with sides: " + c1.a + ", " + c1.b + ", " + c1.c);
+        }
+};
