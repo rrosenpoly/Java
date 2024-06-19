ecdc8eb9c2382a33add77a06df28de9b0bed6fad
rrosenpoly <118927505+rrosenpoly@users.noreply.github.com>
2024-06-19 11:21:20 -0400

    Update Logical.java

--- Lab 1/Logical.java
+++ Lab 1/Logical.java
@@ -1,1 +1,26 @@
+import java.util.Scanner;
 
+public class Logical {
+    public static void main(String[] args) {
+
+        // create Scanner object
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter three integers: ");
+
+        // get input
+        int a = input.nextInt();
+        int b = input.nextInt();
+        int c = input.nextInt();
+
+        // determines
+        boolean divisibleByBoth = (a%b==0) && (a%c==0);
+        boolean divisibleByEither = (a%b==0) || (a%c==0);
+        boolean divisibleByEitherButNotBoth = (a%b==0) ^ (a%c==0);
+
+        // Output
+        System.out.println("Is " + a + " divisible by " + b + " and " + c + "? " + divisibleByBoth);
+        System.out.println("Is " + a + " divisible by " + b + " or " + c + "? " + divisibleByEither);
+        System.out.println("Is " + a + " divisible by " + b + " or " + c + ", but not both? " + divisibleByEitherButNotBoth);
+    }
+
+}
