271be92cf4cd36bbf52967ca5b4a249398f365c3
rrosenpoly <118927505+rrosenpoly@users.noreply.github.com>
2024-06-19 11:20:47 -0400

    Update AnnualTax.java

--- Lab 1/AnnualTax.java
+++ Lab 1/AnnualTax.java
@@ -1,1 +1,39 @@
+import java.util.Scanner;
 
+public class AnnualTax {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter your income and age:");
+        double income = input.nextDouble();
+        double age = input.nextInt();
+
+        // declare variables and initialize
+        double taxRate, taxAmount, seniorDiscountPercentageRate = 0, seniorTaxAmount;
+
+        // calculate tax
+        if (income <= 50000) {
+            taxRate = 0;
+            taxAmount = 0.0;
+        } else if (income <= 100000) {
+            taxRate = 7;
+            taxAmount = income * taxRate;
+        } else {
+            taxRate = 9;
+            taxAmount = income * taxRate;
+        }
+        // calculate tax for senior discount
+        if (taxRate > 0 && age >= 60) {
+            seniorDiscountPercentageRate = taxRate - 3;
+            seniorTaxAmount = income * seniorDiscountPercentageRate;
+            seniorTaxAmount /= 100;
+            System.out.println("Your tax amount is: " + seniorTaxAmount);
+            System.out.println("You are qualified for senior discount. Your senior discount percentage rate is: " + String.format("%.2f" , seniorDiscountPercentageRate) + " %");
+
+        } else {
+            // Output results
+            taxAmount /= 100;
+            System.out.println("Your tax amount is: " + String.format("%.2f", taxAmount));
+            System.out.println("Your tax rate is: " + String.format("%.2f" , taxRate) + " %");
+        }
+    }
+}
