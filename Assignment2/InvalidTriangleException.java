993df68b974f4234c851cdfd6db0124d88dec4d2
rrosenpoly <118927505+rrosenpoly@users.noreply.github.com>
2024-06-19 11:09:16 -0400

    Update InvalidTriangleException.java

--- Assignment2/InvalidTriangleException.java
+++ Assignment2/InvalidTriangleException.java
@@ -1,1 +1,25 @@
+public class InvalidTriangleException extends RuntimeException {
+    private double a, b, c;
 
+    public InvalidTriangleException(){}
+
+    public InvalidTriangleException(double a, double b, double c) {
+        super("Invalid triangle with sides: " + a + ", " + b + ", " + c);
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    // public getters
+    public double getA() {
+        return a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public double getC() {
+        return c;
+    }
+}
