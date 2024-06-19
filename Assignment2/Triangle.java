08b292da9c9080901e84bc38f1a6f158313533e4
rrosenpoly <118927505+rrosenpoly@users.noreply.github.com>
2024-06-19 11:10:35 -0400

    Update Triangle.java

--- Assignment2/Triangle.java
+++ Assignment2/Triangle.java
@@ -1,1 +1,20 @@
 
+public class Triangle extends GeometricObject{
+
+    // declare variables
+    double a, b, c;
+
+    // Constructor
+    public Triangle(double a, double b, double c) throws InvalidTriangleException{
+        // TODO:
+        // if invalid throw InvalidTriangleException
+        // else assign the tree sides respectively
+            if (a + b <= c || a + c <= b || b + c <= a){
+            throw new InvalidTriangleException(a, b, c);
+            }
+                this.a = a;
+                this.b = b;
+                this.c = c;
+            }
+
+};
